38b6bdc94566b4b3308454ae4b1cff073152e1cd
Rozashas <hyunkyu4546@gmail.com>
2026-05-28 14:06:03 +0900

    main 복구 작업

--- Assets/Scripts/ToolAction.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-// 모든 도구 기능들의 부모가 될 추상 클래스
-public abstract class ToolAction : MonoBehaviour
-{
-    // 이 도구를 사용할 때 실행되는 로직 (자식 클래스가 직접 구현)
-    public abstract void ExecuteAction(PlayerMovement player);
-}
